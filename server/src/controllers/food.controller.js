281d999cd387efa215879152f238b7693d1659de
Dinesh <dkkumar97855@gmail.com>
2025-05-28 17:34:55 +0530

    Implement feature X to enhance user experience and optimize performance

--- server/src/controllers/food.controller.js
+++ server/src/controllers/food.controller.js
@@ -9,57 +9,47 @@ const Cloudinary = require("../config/cloudinary.js")
 exports.createFood = asyncHandler(async (req, res) => {
   try {
     const { name, category, price, orderType } = req.body;
-<<<<<<< HEAD
     console.log("Body:", req.body);
 
 console.log("File:", req.file);
-=======
     // console.log("Body:", req.body);
 
 // console.log("File:", req.file);
->>>>>>> 52ed44e3 (feat: Add food category management functionality)
     
 
     const image = req.file?.path; // Cloudinary image URL
 
-<<<<<<< HEAD
+
     if (!name || !category || !price || !image || !orderType) {
-=======
+
     if (!name || !category || !price || !image) {
->>>>>>> 52ed44e3 (feat: Add food category management functionality)
       throw new ApiError(400, "All fields are required including orderType");
     }
 
     // Ensure orderType is always an array
-<<<<<<< HEAD
+
     // const parsedOrderType = Array.isArray(orderType) ? orderType : orderType;
-=======
     const parsedOrderType = Array.isArray(orderType) ? orderType : orderType;
->>>>>>> 52ed44e3 (feat: Add food category management functionality)
+
 
     const food = await Food.create({
       name,
       price,
       category,
       image,
-<<<<<<< HEAD
-      orderType
-=======
       orderType: parsedOrderType,
->>>>>>> 52ed44e3 (feat: Add food category management functionality)
     });
 
     return res.status(201).json(
       new ApiResponse("Food Item created successfully", food)
     );
-  } catch (error) {
+  } }catch (error) {
     throw new ApiError(500, error?.message || "Something went wrong while creating Food Item");
   }
 });
 
 
 // Get  Items
-<<<<<<< HEAD
 exports.getAllFoods = asyncHandler(async (req, res) => {
   try {
     const { page = 1, limit = 10, category, orderType } = req.query;
@@ -67,42 +57,11 @@ exports.getAllFoods = asyncHandler(async (req, res) => {
     const query = {};
 
     if (category) {
-      query.category = category;
-=======
-exports.getAllFoods = asyncHandler (async(req, res) => {
-    try {
-      const { page = 1,  category, orderType } = req.query;
-  
-      const query = {};
-  
-    
-      if (category) {
-        query.category = category; // Match exact category
-      }
-
-      if( orderType) {
-        query.orderType = { $in: Array.isArray(orderType) ? orderType : [orderType] }; // Match any of the order types
-      }
-  
-      const total = await Food.countDocuments(query);
-      const foods = await Food.find(query)
-        .skip(page - 1) 
-        // .limit(parseInt(limit));
-  
-      res.status(200).json({
-        success: true,
-        currentPage: +page,
-        totalPages: Math.ceil(total),
-        // totalItems: total,
-        foods,
-      });
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch foods" });
->>>>>>> 52ed44e3 (feat: Add food category management functionality)
+      query.category = category; // Match exact category
     }
 
     if (orderType) {
-      query.orderType = orderType; // Matches items that include the given orderType
+      query.orderType = { $in: Array.isArray(orderType) ? orderType : [orderType] }; // Match any of the order types
     }
 
     const total = await Food.countDocuments(query);
